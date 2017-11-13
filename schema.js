11b46748735d71409818ed106473e3b44899cd66
buoyantair <buoyantair@gmail.com>
2017-11-13 18:13:02 +0530

    more files

---
+++ schema.js
@@ -0,0 +1,75 @@
+const fetch = require('node-fetch');
+const util = require('util');
+const parseXML = util.promisify(require('xml2js').parseString);
+const {
+    GraphQLSchema,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLObjectType,
+    GraphQLList
+} = require('graphql');
+
+/*
+const x = fetch(
+    'https://www.goodreads.com/author/show.xml?id=4432&key=epw6ZBQesxv99KAvPRd1bg'
+)
+.then(response => response.text())
+.then(parseXML);
+*/
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: '...',
+
+    fields: () => ({
+        title: {
+            type: GraphQLString,
+            resolve: xml => 
+                xml.title[0]
+        },
+        isbn: {
+            type: GraphQLString
+        }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: '...',
+
+    fields: () => ({
+        name: {
+            type: GraphQLString,
+            resolve: xml => 
+                xml.GoodreadsResponse.author[0].name[0]
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: xml => 
+                xml.GoodreadsResponse.author[0].books[0].book
+        }
+    })
+})
+
+module.exports = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        desc: '...',
+
+        fields: () => ({
+            author: {
+                type: AuthorType,
+                args: {
+                    id: {
+                        type: GraphQLInt
+                    }
+                },
+                resolve: (root, args) =>  fetch(
+                    `https://www.goodreads.com/author/show.xml?id=${args.id}&key=epw6ZBQesxv99KAvPRd1bg`
+                )
+                .then(response => response.text())
+                .then(parseXML)
+            }
+        })
+    })
+})
